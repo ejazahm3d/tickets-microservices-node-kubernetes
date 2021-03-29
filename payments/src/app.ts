b45832a60611d2b5867e84cb7f7e3582faba6aa8
Ejaz Ahmed <ejazahmed.dev@gmail.com>
2021-03-29 09:45:49 +0500

    created new route for payments service and configured ingress controller to handle the routing

--- payments/src/app.ts
+++ payments/src/app.ts
@@ -4,6 +4,7 @@ import { json } from "body-parser";
 import cookieSession from "cookie-session";
 import { currentUser, errorHandler } from "@sgtickets/common";
 import { NotFoundError } from "@sgtickets/common";
+import { CreateChargeRouter } from "./routes/new";
 
 const app = express();
 app.set("trust proxy", true);
@@ -17,6 +18,8 @@ app.use(
 );
 
 app.use(currentUser);
+
+app.use(CreateChargeRouter);
 
 app.all("*", async () => {
   throw new NotFoundError();
---
+++ payments/src/routes/new.ts
@@ -0,0 +1,18 @@
+import { requireAuth, validateRequest } from "@sgtickets/common";
+import express, { Request, Response } from "express";
+import { body } from "express-validator";
+import { Order } from "../models/order";
+
+const router = express.Router();
+
+router.post(
+  "/api/payments",
+  requireAuth,
+  [body("token").not().isEmpty(), body("orderId").not().isEmpty()],
+  validateRequest,
+  (req: Request, res: Response) => {
+    res.send({ success: true });
+  }
+);
+
+export { router as CreateChargeRouter };
